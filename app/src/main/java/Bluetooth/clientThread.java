a39796ad13602d59edaa911b4893efa44d242490
svensgaard <hypermads@gmail.com>
2016-04-06 12:19:01 +0200

    More bluetooth

---
+++ app/src/main/java/Bluetooth/clientThread.java
@@ -0,0 +1,10 @@
+package Bluetooth;
+
+/**
+ * Created by Mads on 30-03-2016.
+ */
+public class ClientThread extends Thread {
+    public void run() {
+
+    }
+}
---
+++ app/src/main/java/Bluetooth/serverThread.java
@@ -0,0 +1,11 @@
+package Bluetooth;
+
+/**
+ * Created by Mads on 30-03-2016.
+ */
+public class ServerThread extends Thread{
+    @Override
+    public void run() {
+
+    }
+}
